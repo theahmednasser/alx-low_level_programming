1d149353fa74371821be9cd621a48e657572a9c3
Ahmed Nasser <ahmed.nasserm20s@gmail.com>
2023-03-21 11:55:31 -0700

    lol

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,11 +5,17 @@
  * main - Entry point
  *
  * Return: Always 0 (Success)
+ *
+ * print_alphabet - prints all letters in lowercase 
+ *
+ * Return: no return
  */
 
 void print_alphabet(void)
 {
 	int i;
+
 	for (i = 97; i <= 122; i++)
-		printf("%c\n", i);
+		_putchar(i);
+	printf("\n");
 }
