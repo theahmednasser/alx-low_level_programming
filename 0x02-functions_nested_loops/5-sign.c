55e766bf834e6a4b6d49551e06b503cba48dca25
Ahmed Nasser <ahmed.nasserm20s@gmail.com>
2023-03-21 16:11:17 -0700

    lol

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - function that prints sign of a given input
+ *
+ * @n: integer input
+ *
+ * Return: 1 if n is +ve, 0 if n is zero and -1 if n is -ve
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		return (1);
+		_putchar('+');
+	}
+	if (n == 0)
+	{
+		return (0);
+		_putchar('0');
+	}
+	if (n < 0)
+	{
+		return (-1);
+		_putchar('-');
+	}
+}
