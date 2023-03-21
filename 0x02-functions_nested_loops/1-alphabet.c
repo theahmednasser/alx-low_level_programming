eee971e8585dd42c2a9f52b0141f714d40756518
Ahmed Nasser <ahmed.nasserm20s@gmail.com>
2023-03-21 11:50:03 -0700

    lol

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+	for (int i = 97; i <= 122; i++)
+		printf("%c\n", i);
+}
